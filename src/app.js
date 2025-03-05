2e6f74789d15574effbc17279a2c836cb98f6bdb
chandan keshari <chandankeshari93@gmail.com>
2025-03-05 20:34:14 +0530

    initializing git folder

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use("/test", (req, res) => {
+   res.send("hii");
+});
+
+app.listen(3000, () => {
+   console.log("Listening on Port: 3000");
+});
